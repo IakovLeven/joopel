30b7b67f3fa62fa0036baec1a99ed2925bafab42
Яков Левен <iakovleven@192.168.1.13>
2022-08-08 21:07:52 +0300

    SETTING UP PROJECT

---
+++ Joopel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Joopel
+//
+//  Created by Яков Левен on 08.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
